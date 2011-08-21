36dd00fc8baf5193e69ef3f2aad19ec4d8d96457
Dann Luciano <dannluciano@gmail.com>
2011-08-21 00:23:29 -0300

    Finished the migration site for rails 3.0.9

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,32 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function change_language(lang) {
+  if (lang === null) {
+    
+  } else {
+    I18n.locale = lang;
+  }
+  
+  $("#title").html(I18n.t("title"));
+  $("#subtitle").html(I18n.t("subtitle"));
+  $("#about").html(I18n.t("about"));
+  $("#blog").html(I18n.t("blog"));
+  $("#contact").html(I18n.t("contact"));
+}
+change_language();
+
+$("#en").click(function() {
+  change_language("en");
+});
+
+$("#pt-br").click(function() {
+  change_language("pt-br");
+});
+
+$.facebox.settings.closeImage = '/images/closelabel.png'
+$.facebox.settings.loadingImage = '/images/loading.gif'
+
+jQuery(document).ready(function($) {
+  $('a[rel*=facebox]').facebox()
+})
